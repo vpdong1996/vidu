5c4f68dc0589d3bf3de749b59f7bbcaa0aaa2682
unknown <dongkg3876@gmail.com>
2017-10-02 16:36:59 +0700

    init with git

---
+++ public/progress.js
@@ -0,0 +1,26 @@
+var BaiTap = React.createClass({
+  render : function(){
+      return(
+        <div>
+        <h1 className= "xanh" > {this.props.vidu2} </h1>
+        <Vidu />
+        </div>
+      );
+
+  }
+});
+var Vidu = React.createClass({
+  render : function(){
+    return(
+      <h2 className="mau2"> Em </h2>
+    );
+  }
+});
+ReactDOM.render (
+  <div>
+  <BaiTap vidu2="Example" />
+  <hr/>
+  <BaiTap vidu2="EXP"/>
+
+  </div>
+  , document.getElementById("root"));
